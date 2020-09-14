568635f346e203aa78f7bb9eadf20073e592cbe5
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-14 10:36:07 -0400

    initial look of sidebar and background

---
+++ whatsapp-clone/src/Sidebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="siderbar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCZJZPMt0nvyLECppytC4RVd9mp_weL3YE",
+  authDomain: "whatsapp-mern-2fc96.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-2fc96.firebaseio.com",
+  projectId: "whatsapp-mern-2fc96",
+  storageBucket: "whatsapp-mern-2fc96.appspot.com",
+  messagingSenderId: "1080624628287",
+  appId: "1:1080624628287:web:89da2267b7d4bbdc2a6e8a",
+  measurementId: "G-JTNY3NMM9F",
+};
