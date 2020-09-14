26384343ee43454799498a90bc96682913deed6d
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-14 13:44:40 -0400

    chat initial

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -1,8 +1,41 @@
 import React from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 
 function Chat() {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>last seen at ..</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p>
+          <span className="chat__name">Sonny</span>
+          message ...
+          <span className="chat__timestamp"> {new Date().toUTCString()} </span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -4,6 +4,8 @@ import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
   return (
@@ -21,6 +23,19 @@ function Sidebar() {
             <MoreVertIcon />
           </IconButton>
         </div>
+      </div>
+
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div>
   );
