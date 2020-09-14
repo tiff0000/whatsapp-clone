ab8144c41526e567d70c3964d125664dc87e2f3a
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-14 14:42:18 -0400

    frontend done

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
-
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 function Chat() {
   return (
     <div className="chat">
+      {/* ------ start chat header  ------*/}
       <div className="chat__header">
         <Avatar />
 
@@ -26,14 +28,40 @@ function Chat() {
           </IconButton>
         </div>
       </div>
+      {/* ------ END chat header  ------*/}
 
+      {/* ------ start chat body  ------*/}
       <div className="chat__body">
-        <p>
+        <p className="chat__message">
+          <span className="chat__name">Sonny</span>
+          message ...
+          <span className="chat__timestamp"> {new Date().toUTCString()} </span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Sonny</span>
+          message ...
+          <span className="chat__timestamp"> {new Date().toUTCString()} </span>
+        </p>
+
+        <p className="chat__message chat__receiver">
           <span className="chat__name">Sonny</span>
           message ...
           <span className="chat__timestamp"> {new Date().toUTCString()} </span>
         </p>
       </div>
+      {/* ------ END chat body  ------*/}
+
+      {/* ------ START chat footer  ------*/}
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input type="text" placeholder="Type a message" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
+      {/* ------ END chat footer  ------*/}
     </div>
   );
 }
