6f291b6b9cf4e07efb06eb1c7b801e9b0cec3ed6
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-15 05:18:33 -0400

    added chat function with input from user. app  app done

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  //fetch with axios
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios.get("/messages/sync").then((res) => {
+      setMessages(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    var pusher = new Pusher("f93c635b69e51e1e0e44", {
+      cluster: "us2",
+    });
+
+    var channel = pusher.subscribe("messages");
+    channel.bind("inserted", (newMessage) => {
+      // alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        <Chat />
+        <Chat messages={messages} />
       </div>
     </div>
   );
--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
-function Chat() {
+import axios from "./axios";
+
+function Chat({ messages }) {
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    await axios.post("./messages/new", {
+      message: input,
+      name: "Tiffany",
+      timestamp: "Just now",
+      received: true,
+    });
+
+    setInput("");
+  };
+
   return (
     <div className="chat">
       {/* ------ start chat header  ------*/}
@@ -32,23 +49,15 @@ function Chat() {
 
       {/* ------ start chat body  ------*/}
       <div className="chat__body">
-        <p className="chat__message">
-          <span className="chat__name">Sonny</span>
-          message ...
-          <span className="chat__timestamp"> {new Date().toUTCString()} </span>
-        </p>
-
-        <p className="chat__message">
-          <span className="chat__name">Sonny</span>
-          message ...
-          <span className="chat__timestamp"> {new Date().toUTCString()} </span>
-        </p>
-
-        <p className="chat__message chat__receiver">
-          <span className="chat__name">Sonny</span>
-          message ...
-          <span className="chat__timestamp"> {new Date().toUTCString()} </span>
-        </p>
+        {messages.map((message) => (
+          <p
+            className={`chat__message ${message.received && "chat__receiver"} `}
+          >
+            <span className="chat__name">{message.name}</span>
+            {message.message}
+            <span className="chat__timestamp">{message.timestamp}</span>
+          </p>
+        ))}
       </div>
       {/* ------ END chat body  ------*/}
 
@@ -56,8 +65,15 @@ function Chat() {
       <div className="chat__footer">
         <InsertEmoticonIcon />
         <form>
-          <input type="text" placeholder="Type a message" />
-          <button type="submit">Send a message</button>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Type a message"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
         </form>
         <MicIcon />
       </div>
